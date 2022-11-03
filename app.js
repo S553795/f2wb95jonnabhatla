b17bbbf2d619f98785b619a95754932996f9038c
satya <satyajonnabhatla98@gmail.com>
2022-11-02 19:31:05 -0500

    added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var engineRouter = require('./routes/engines');
+var gridBuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/engines', engineRouter);
+app.use('/gridbuild', gridBuildRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
